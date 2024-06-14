ae078afa125eb9a19b216f849ca470fd9f32817e
KrisYangPO <kristoff0109@gmail.com>
2024-06-14 12:13:24 +0800

    testxlsx2csv

--- pl_xlsx2csv.sh
+++ pl_xlsx2csv.sh
@@ -7,12 +7,7 @@ cd ${path}
 file=sampleFormula_test.xlsx
 outputname=sampleFormula_test.csv
 
-<<<<<<< HEAD
-/staging/biology/ls807terra/0_Programs/xlsx2csv-master/xlsx2csv.py \
- --delimiter 'tab' \
-=======
 xlsx2csv.py --delimiter 'tab' \
->>>>>>> 818eacc (Develop Sample Table input pipeline)
  ${file} \
  ${outputname}
 
